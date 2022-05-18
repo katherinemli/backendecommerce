e843d2a7caf1ecf0f6b1d80909a81dcd805ef295
katherinemli <katherine.lib.ira@gmail.com>
2022-05-18 17:35:26 -0400

    first commit

--- inventory/models.py
+++ inventory/models.py
@@ -9,6 +9,11 @@ class Product(models.Model):
     price = models.FloatField(null=True)
     inventory_left = models.IntegerField(null=True)
 
+    def save(self, *args, **kwargs):
+        inventory = Product.objects.get(id=self.id)
+        self.inventory_left = inventory.inventory_left - 1
+        super(Product, self).save(*args, **kwargs)
+
     class Meta:
         ordering = ["id"]
 
@@ -17,6 +22,14 @@ class Coupon(models.Model):
     name = models.CharField(max_length=250, null=True)
     discount = models.FloatField(null=True)
     times_used = models.IntegerField(null=True)
+
+    def save(self, *args, **kwargs):
+        coupon = Coupon.objects.get(id=self.id)
+        if coupon.times_used is None:
+            self.times_used = 1
+        else:
+            self.times_used = coupon.times_used + 1
+        super(Coupon, self).save(*args, **kwargs)
 
     class Meta:
         ordering = ["discount"]
